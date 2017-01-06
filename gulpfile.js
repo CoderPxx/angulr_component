a44effbaf0e905461be0fba0e1253836f496bda5
pengxiaoxiao <pxx@mac.chinasoftinc.com>
2017-01-06 14:28:22 +0800

    delete buttons on navigation bar and delete someof buttons

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
